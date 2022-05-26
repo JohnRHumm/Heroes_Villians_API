9d47ae06baa8dc875693c9143700a284588a6ed6
Humm <jhumm@mcw.edu>
2022-05-25 21:28:22 -0500

    feat:Created supers app + Super class

--- super_types/models.py
+++ super_types/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 
-class SuperTypes(models.Model):
-    isHero = models.BooleanField
+class SuperType(models.Model):
+    type = models.CharField(max_length=255)
